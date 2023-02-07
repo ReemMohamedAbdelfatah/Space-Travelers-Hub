b108323ad98f453d25dbed9de79aa804895c3dfa
Fahim-Khan-P <fahim.tanvir2021@gmail.com>
2023-02-07 22:34:02 +0430

    add rocket cancelation property in  Rocket.js file!

--- src/components/Rocket.js
+++ src/components/Rocket.js
@@ -17,12 +17,19 @@ const Rocket = (rocket) => {
       <img className="rocketImage" alt="rocket..." src={flickrImages} />
       <div className="rocketDetails">
         <h2 className="rocketName">{rocketName}</h2>
-        <p className="rocketDescription">{description}</p>
+        <p className="rocketDescription">
+          <span
+            className={reserved ? 'show' : 'hide'}
+          >
+            Reserved
+          </span>
+          {description}
+        </p>
 
         <button
           type="button"
-          className="reserveBtn"
           onClick={handleReserve}
+          className={reserved ? 'cancelReserved' : 'reserveBtn'}
         >
           {reserved ? 'Cancel Reservation' : 'Reserve Rocket'}
         </button>
